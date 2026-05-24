8bb2de24510693aa80394ae1d8eddbbfbd5b98fd
David <dgarcia123david@gmail.com>
2026-05-23 22:21:11 -0500

    Move database credentials to environment variables

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "newpassword",
-  database: "workforce",
+  host: process.env.DB_HOST,
+  user: process.env.DB.USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
 });
