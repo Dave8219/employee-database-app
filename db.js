b37c152387a9adc50f64006e1c51e9c4d54f618d
David <dgarcia123david@gmail.com>
2026-05-27 13:48:10 -0500

    replaced .env backend with AWS info - away form local server and into AWS server database

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  user: process.env.DB.USER,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
